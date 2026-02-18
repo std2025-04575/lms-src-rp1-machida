b2f17f56076f4622ef895ee544bcdf20706ab656
admin <admin@192.168.0.44>
2026-02-18 11:09:13 +0900

    add:町田　Task.26　勤怠管理直接変更画面　フィールドを追加
    数値型で抜き出した時間、分、のフィールドを作成した。

--- trial_source/src/main/java/jp/co/sss/lms/form/DailyAttendanceForm.java
+++ trial_source/src/main/java/jp/co/sss/lms/form/DailyAttendanceForm.java
@@ -18,8 +18,16 @@ public class DailyAttendanceForm {
 	private String trainingDate;
 	/** 出勤時間 */
 	private String trainingStartTime;
+	/** 出勤時間(時間) */
+	private Integer trainingStartHour;
+	/** 出勤時間(分) */
+	private Integer trainingStartMinute;
 	/** 退勤時間 */
 	private String trainingEndTime;
+	/** 退勤時間(時間) */
+	private Integer trainingEndHour;
+	/** 退勤時間(分) */
+	private Integer trainingEndMinute;
 	/** 中抜け時間 */
 	private Integer blankTime;
 	/** 中抜け時間（画面表示用） */
